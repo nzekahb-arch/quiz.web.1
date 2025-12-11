b6e5d9ee43158121eb2a9cb1b485676c4d7480bb
nzekahb-arch <nzekahb@gmail.com>
2025-12-11 22:19:02 +0100

    Add files via upload

---
+++ quiz website/questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "micro-organisms are found --------",
+        answer: "B. Everywhere",
+        options: [
+            "A. On Islands",
+            "B. Everywhere",
+            "C. Only in water",
+            "D. Only on Land"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the term for the movement of genes from one population to another?",
+        answer: "B. Gene flow",
+        options: [
+            "A. Genetic drift",
+            "B. Gene flow",
+             "C. Mutation",
+             "D. Natural selection"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the name of the process by which plants convert sunlight into energy?",
+        answer: "B. Photosynthesis",
+        options: [
+            "A. Respiration",
+            "B. Photosynthesis",
+            "C. Decomposition",
+            "D. Fermentation",
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who is credited with the development of the theory of evolution through natural selection?",
+        answer: "A. Charles Darwin",
+        options: [
+            "A. Charles Darwin",
+            "B. Gregor Mendel",
+            "C. Louis Pasteur",
+            "D. James Watson"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the primary function of the mitochondria in a cell?",
+        answer: "C. Energy production",
+        options: [
+            "A. Protein synthesis",
+            "B. DNA replication",
+            "C. Energy production",
+            "D. Waste removal"
+
+        ]
+    }
+];
